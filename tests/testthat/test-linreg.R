0b016e7c3ae19e75e65c4b9aed73f96aa3c4d2ff
Andy Shi <andyshi@g.harvard.edu>
2019-12-03 10:02:03 -0500

    Add tests.

---
+++ tests/testthat/test-linreg.R
@@ -0,0 +1,14 @@
+test_that("beta on average is correct", {
+  alpha_true <- 1
+  beta_true <- 0.5
+  beta_sim <- linreg_sim(1000, 100, alpha_true, beta_true)
+  expect_equal(mean(beta_sim), beta_true)
+})
+
+test_that("get_lm_components is returning correctly", {
+  x <- rnorm(100)
+  y <- 0.2 + 0.1 * x + rnorm(100)
+  mod <- lm(y ~ x)
+  beta <- get_lm_components(mod)
+  expect_identical(names(beta), "x")
+})
