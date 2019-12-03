00c54f7484b58f375b0696e710d703bca6442c27
Andy Shi <andyshi@g.harvard.edu>
2019-12-03 10:20:04 -0500

    add functional programming

--- R/linreg.R
+++ R/linreg.R
@@ -2,11 +2,12 @@ get_lm_components <- function(lm_obj) {
   return(coef(lm_obj)[2])
 }
 
-linreg_sim <- function(nsims, nsamp, alpha_true, beta_true) {
+linreg_sim <- function(nsims, nsamp, alpha_true, beta_true,
+                       err_func = rnorm) {
   x <- rnorm(nsamp)
   beta <- c()
   for (i in 1:nsims) {
-    errors <- rnorm(nsamp)
+    errors <- err_func(nsamp)
     y <- alpha_true + beta_true * x + errors
     mod <- lm(y ~ x)
     beta_cur <- get_lm_components(mod)
